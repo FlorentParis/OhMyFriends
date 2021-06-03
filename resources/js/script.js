f91b6271e804048ced17cc43b60eebf3fcecd5c5
Florent <florentparis28@gmail.com>
2021-06-03 17:44:36 +0200

    init

---
+++ resources/js/script.js
@@ -0,0 +1,62 @@
+import { DragControls } from '../../three.js/examples/jsm/controls/DragControls.js';
+
+/* Moteur de Rendu / Canvas */
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+/* Camera */
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.5, 1000 );
+
+/* Scene */
+const scene = new THREE.Scene();
+
+/* Mise en place */
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+/* Redimentionnement Auto Canvas */
+function updateViewportSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener("resize", updateViewportSize);
+updateViewportSize();
+
+/* Cube */
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+camera.position.z = 5;
+const animate = function () {
+    requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+};
+animate();
+
+/* Sphere */
+const geometry2 = new THREE.SphereGeometry( 5, 32, 32 );
+const material2 = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+const sphere = new THREE.Mesh( geometry2, material2 );
+scene.add( sphere );
+
+/* liste des objets pour l'interaction */
+var objects = [];
+objects.push(cube);
+objects.push(sphere);
+
+/* Controles */
+var controls = new DragControls( objects, camera, renderer.domElement );
+controls.addEventListener( 'dragstart', function ( event ) {
+	event.object.material.color = new THREE.Color(0xff0000) ;
+});
+controls.addEventListener( 'dragend', function ( event ) {
+	event.object.material.color = new THREE.Color(0x00ff00);
+});
+
+/* Rendu */
+renderer.render(scene, camera);
---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Reset CSS -->
+    <link rel="stylesheet" href="https://meyerweb.com/eric/tools/css/reset/reset.css">
+    <!-- Fichier CSS -->
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- Fichier JS Source -->
+    <script src="../../three.js/build/three.js" defer></script>
+    <!-- Fichier JS -->
+    <script type="module" src="../js/script.js" defer></script>
+    <title>Oh My Friends !</title>
+</head>
+<body>
+    
+</body>
+</html>
