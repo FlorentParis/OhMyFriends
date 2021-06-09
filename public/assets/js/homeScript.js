c0d9d03b4b6e0724cee6beccd25753870fcccf00
PiKwaii93 <maxence.nho@hetic.net>
2021-06-09 10:47:13 +0200

    graphique

--- public/assets/js/homeScript.js
+++ public/assets/js/homeScript.js
@@ -47,18 +47,23 @@ function courbe(courbe){
   }
 }
 
-points_y = [18, 27, 23, 25, 31, 37, 35, 26, 17, 25];
+Rachel = [18, 27, 23, 25, 31, 37, 35, 26, 17, 25];
+Monica = [14,27,23,31,16,25,19,20,28,36];
+Phoebe = [8,18,24,13,15,15,25,19,28,9];
+Ross =[6,9,6,8,9,12,12,9,25,21];
+Chandler = [9,3,7,7,11,18,8,7,4,5];
+Joey=[3,6,4,4,5,9,4,13,9,7];
 
-function testpoint(points){
+function testpoint(points, color){
   ctx.beginPath();
-  ctx.lineWidth = "3";
-  ctx.strokeStyle="red";
+  ctx.lineWidth = "1";
+  ctx.strokeStyle=color;
   var base = 577;
   var x = 50;
-  ctx.moveTo(x,base - points[0]*10);
+  ctx.moveTo(x,base - points[0]*12);
   for(let z = 0; z<points.length; z++){
-    ctx.lineTo(x, base - points[z]*10);
-    x = x+83.33;
+    ctx.lineTo(x, base - points[z]*12);
+    x = x+84;
   }
   ctx.stroke();
 }
@@ -66,37 +71,37 @@ function testpoint(points){
 function testGrilleVertical(){
   var x = 50;
   var base = 577;
-  var fin = 50;
+  var fin = 37;
   for(let z = 0; z<10;z++){
     ctx.beginPath();
-    ctx.lineWidth = "0.5";
+    ctx.lineWidth = "0.25";
     ctx.strokeStyle="black";
     ctx.moveTo(x,base);
     ctx.lineTo(x,fin);
     ctx.stroke();
-    x = x +83.33;
+    x = x + 84;
   }
 }
 
 function testGrilleHorizontal(){
   var y = 577;
   var base = 50;
-  var fin = 800
+  var fin = 806
   for(let z = 0; z<10; z++){
     ctx.beginPath();
-    ctx.lineWidth = "0.5";
+    ctx.lineWidth = "0.25";
     ctx.strokeStyle="black";
     ctx.moveTo(base,y);
     ctx.lineTo(fin,y);
     ctx.stroke();
-    y = y - 83.33;
+    y = y - 60;
   }
 }
 
 function testAbsisse(){
   var y = 577;
   var base = 50;
-  var fin = 800;
+  var fin = 806;
   ctx.beginPath();
   ctx.lineWidth = "1.5";
   ctx.strokeStyle="black";
@@ -113,7 +118,7 @@ function testAbsisse(){
 function testOrdonnee(){
   var x = 50;
   var base = 577;
-  var fin = 50;
+  var fin = 37;
   ctx.beginPath();
   ctx.lineWidth = "1.5";
   ctx.strokeStyle="black";
@@ -131,7 +136,7 @@ function testOrdonnee(){
 function testUniteOrdonnee(){
   var x = 50;
   var base = 577;
-  for(let z = 0; z<32; z++){
+  for(let z = 0; z<45; z++){
     if(z%5 == 0){
       ctx.beginPath();
       ctx.strokeStyle = "black";
@@ -139,7 +144,7 @@ function testUniteOrdonnee(){
       ctx.moveTo(x, base);
       ctx.lineTo(x-10, base);
       ctx.stroke();
-      base = base - 16.666;
+      base = base - 12;
     }else{
       ctx.beginPath();
       ctx.strokeStyle = "black";
@@ -147,7 +152,7 @@ function testUniteOrdonnee(){
       ctx.moveTo(x, base);
       ctx.lineTo(x-5, base);
       ctx.stroke();
-      base = base - 16.666;
+      base = base - 12;
     }
     
   }
@@ -158,56 +163,85 @@ function testUniteAbcisse(){
   var x = 50;
   var base = 577;
   for(let z = 0; z<45; z++){
-    if(z%5 == 0){
+    if(z%5==0){
       ctx.beginPath();
       ctx.strokeStyle = "black";
       ctx.lineWidth = "2";
-      ctx.moveTo(x, base);
-      ctx.lineTo(x, base+10);
+      ctx.moveTo(x,base);
+      ctx.lineTo(x, base + 10);
       ctx.stroke();
-      x = x + 16.666;
-    }else{
-      ctx.beginPath();
-      ctx.strokeStyle = "black";
-      ctx.lineWidth = "1";
-      ctx.moveTo(x, base);
-      ctx.lineTo(x, base+5);
-      ctx.stroke();
-      x = x + 16.666;
+      x = x + 84;
     }
-    
   }
-
 }
 
 function testNumberAbcisse(){
   var x = 47;
-  var base = 600;
-  var base2= 605;
-  var x2 = 47;
-  for(let z = 0; z<45; z++){
-    if(z%5==0){
-      x = x + 16.6;
+  base = 600;
+  for(let z = 1; z<11; z++){
+    if(z>8){
+      text = z.toString();
+      ctx.lineWidth="1.1";
+      ctx.font = '16px serif';
+      ctx.strokeText(text, x, base);
+      x = x + 80;
     }else{
       text = z.toString();
-      ctx.font = '12px serif';
+      ctx.lineWidth="1.1";
+      ctx.font = '16px serif';
       ctx.strokeText(text, x, base);
-      x= x + 16.6;
-    }
-  }
-  for(let z = 0; z<45; z++){
-    if(z%5==0){
-      text = z.toString();
-      ctx.font = '16px serirf';
-      ctx.strokeText(text, x2, base2);
-      if(z<9){
-        x2 = x2 + 82;
-      }else{
-        x2 = x2 + 83.1;
-      }
+      x = x + 83.9;
     }
   }
 }
+
+function testNumberOrdonnee(){
+  var x = 22.5;
+  base = 582;
+  for(let z = 0; z<45; z++){
+    if(z%5==0){
+      text = z.toString();
+      ctx.lineWidth="1.1";
+      ctx.font = '14px serif';
+      ctx.strokeText(text,x,base);
+      base = base - 60;
+    }
+    
+  }
+}
+
+function titre(){
+  ctx.font='20px serif';
+  ctx.lineWidth="1";
+  text = "Evolution de la quantité de “Oh My God” au fil des saisons pour chaque personnage"
+  ctx.strokeStyle="black";
+  var x = 90;
+  var y = 25;
+  ctx.strokeText(text,x,y);
+}
+
+function saison(){
+  ctx.font='16px serif';
+  ctx.lineWidth="1";
+  text = "Saisons"
+  ctx.strokeStyle="black";
+  var x = 760;
+  var y = 620;
+  ctx.strokeText(text,x,y);
+}
+
+function quantite(){
+  ctx.font='16px serif';
+  ctx.lineWidth="1";
+  text = 'Quantité de "Oh my God"';
+  ctx.strokeStyle="black";
+  ctx.rotate(-90* Math.PI / 180);
+  var x = 0;
+  var y = 0;
+  ctx.translate(-200,12);
+  ctx.strokeText(text,x,y);
+}
+
 
 
 
@@ -218,4 +252,13 @@ testAbsisse()
 testUniteOrdonnee();
 testUniteAbcisse();
 testNumberAbcisse();
-testpoint(points_y);
+testNumberOrdonnee()
+testpoint(Rachel, "#FF0000");
+testpoint(Monica, "#0075FF");
+testpoint(Phoebe, "#009F10");
+testpoint(Ross, "#FFC700");
+testpoint(Chandler, "#EB00FF");
+testpoint(Joey, "#000AFF");
+titre();
+saison();
+quantite();
